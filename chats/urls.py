a03649a0da952131de4b66e31f820e22f981f1a5
Hyunho Lee <ihyeonho@example.com>
2026-01-15 01:59:33 +0900

    작업 내용 커밋

--- chats/urls.py
+++ chats/urls.py
@@ -4,6 +4,8 @@ from .views import ChatAIView  # 폴더 구조에 맞춰 import
 
 urlpatterns = [
     # 예: http://127.0.0.1:8000/api/sessions/1/chat/
+    path('/',Create_session.as_view(), name='create_session'),
     path('sessions/<int:session_id>/chat/', ChatAIView.as_view(), name='chat-post'),
     path('sessions/chats/<message_id>', ChatAIView.as_view(), name='chat-patch'),
+
 ]
