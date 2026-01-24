074678eaa1a01628d6b953ef5da0c5c01bb72c8b
wnswp1122 <wnswp1122@naver.com>
2026-01-24 14:28:52 +0900

    test

--- cases/urls.py
+++ cases/urls.py
@@ -3,6 +3,6 @@ from .views import CaseSearchView, PrecedentDetailView, CaseAnswerView
 
 urlpatterns = [
     path('', CaseSearchView.as_view(), name='case_search'),
-    path('<int:case_id>/answer/', CaseAnswerView.as_view(), name='case_answer'),
-    path('<int:cases_id>/<str:precedents_id>/', PrecedentDetailView.as_view(), name='precedent_detail'),
+    path('<str:precedents_id>/answer/', CaseAnswerView.as_view(), name='case_answer'),
+    path('<str:precedents_id>/', PrecedentDetailView.as_view(), name='precedent_detail'),
 ]
