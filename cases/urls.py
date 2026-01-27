5f5425ef51de571056f422bc0bbb07e2878dbbb0
wnswp1122 <148195782+wnswp1122@users.noreply.github.com>
2026-01-28 05:01:17 +0900

    Update urls.py

--- cases/urls.py
+++ cases/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', CaseSearchView.as_view(), name='case_search'),
-    path('init/', InitDataAPIView.as_view(), name='init-data'),
+    # 'init/' 대신 정규표현식을 사용하여 슬래시 유무에 상관없이 대응
+    re_path(r'^init/?$', InitDataAPIView.as_view(), name='init-data'), 
     path('<str:precedents_id>/', PrecedentDetailView.as_view(), name='precedent_detail'),
     path('answer/<str:precedents_id>/', CaseAnswerView.as_view(), name='case_answer'),
-
 ]
