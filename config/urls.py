7b96edc2071fe3a2e5d3e0bf350d0566faf37e15
wnswp1122 <148195782+wnswp1122@users.noreply.github.com>
2026-01-28 02:08:58 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger', RedirectView.as_view(url='/swagger/', permanent=False), name='swagger-redirect'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Prometheus metrics
+    path('', include('django_prometheus.urls')),  # /metrics 엔드포인트 노출
     # Cases
     path('cases/', include('cases.urls')),
     path('documents/', include('documents.urls'))
