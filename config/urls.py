7731ae515cc8874bf3af4048cd4e24ab3cb046b0
wnswp1122 <148195782+wnswp1122@users.noreply.github.com>
2026-01-28 03:45:31 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,19 +19,15 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
+# config/urls.py (메인)
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Swagger & Redoc
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # 'api/v1/'을 제거하여 프론트엔드 요청 경로와 일치시킴
+    path('cases/', include('cases.urls')),
+    path('documents/', include('documents.urls')),
+    
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    
-    # API v1 (프론트엔드 요청 경로와 일치시켜주세요)
-    path('api/v1/cases/', include('cases.urls')),
-    path('api/v1/documents/', include('documents.urls')),
-    
-    # Prometheus Metrics
     path('metrics/', include('django_prometheus.urls')),
 ]
 
